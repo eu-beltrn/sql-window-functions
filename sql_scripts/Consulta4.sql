387ceef4a742b95b5ca7d144c5e352fb9f9cc628
Eunice-projects <beltraneunice05@gmail.com>
2026-05-18 17:57:16 -0600

    Modificando conuslta 4

--- sql_scripts/Consulta4.sql
+++ sql_scripts/Consulta4.sql
@@ -1,21 +1,13 @@
 use empresa_window_functions;
 
 -- Consulta 4. Comparación de cada venta contra la venta anterior del mismo empleado.
-select 
-    vendedor,
-    fecha_venta,
-    total_venta,
-    venta_anterior,
-    (total_venta - venta_anterior) as dif_anterior
-from (
-    select
-        e.nombre_empleado as vendedor,
-        v.fecha_venta,
-        v.total_venta,
-        lag(v.total_venta) over(
-            partition by e.nombre_empleado
-            order by v.id_venta
-        ) as venta_anterior
-    from ventas v
-    join empleados e on e.id_empleado = v.id_empleado
-) as t;
+select
+    e.nombre_empleado as vendedor,
+    v.fecha_venta,
+    v.total_venta,
+    lag(v.total_venta) over(
+        partition by e.nombre_empleado
+        order by v.id_venta
+    ) as venta_anterior
+from ventas v
+join empleados e on e.id_empleado = v.id_empleado;
